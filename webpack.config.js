f06152fdf756bbe3b9c446f8f7239d82db6e6b1e
Magadanov <magadanov03@mail.ru>
2024-04-19 17:29:36 +0500

    init: start app

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require("path");
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const EslintPlugin = require("eslint-webpack-plugin");
+
+const baseConfig = {
+    entry: path.resolve(__dirname, "./src/index"),
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/i,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "babel-loader",
+                    },
+                ],
+            },
+            {
+                test: /\.(sa|sc|c)ss$/i,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                use: ["file-loader"],
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/i,
+                type: "asset/inline",
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Async Race",
+        }),
+        new EslintPlugin({ extensions: [".ts", ".tsx", ".js", ".jsx"] }),
+        new CleanWebpackPlugin(),
+    ],
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".jsx"],
+    },
+    output: {
+        filename: "index.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    devtool: "inline-source-map",
+    devServer: {
+        static: path.resolve(__dirname, "./dist"),
+        hot: true,
+    },
+};
+
+module.exports = ({ mode }) => {
+    const isProductionMode = mode === "prod";
+    const envConfig = isProductionMode
+        ? require("./webpack.prod.config")
+        : require("./webpack.dev.config");
+
+    return merge(baseConfig, envConfig);
+};
