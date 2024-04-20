480a893b80a1d4d5e9ce348ab256f0bcb5468bc1
Magadanov <magadanov03@mail.ru>
2024-04-21 01:44:10 +0500

    feat: add winner view content

--- src/pages/WinnersPage/WinnersPage.tsx
+++ src/pages/WinnersPage/WinnersPage.tsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import ViewHeader from '../../components/ViewHeader/ViewHeader';
+import usePaginate from '../../utils/usePaginate';
 
 export default function WinnersPage() {
-    return <div>Winners view</div>;
+    const pageLimit = 10;
+    const { data, page, nextPage, prevPage, total } = usePaginate(
+        'winners',
+        pageLimit
+    );
+    console.log(data, nextPage, prevPage);
+
+    return (
+        <>
+            <div className="content-header">
+                <ViewHeader
+                    pageName="Winners"
+                    currentPageNumber={page}
+                    totalItems={total}
+                />
+            </div>
+        </>
+    );
 }
